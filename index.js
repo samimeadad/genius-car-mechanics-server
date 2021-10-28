c3f2d2319a46f3f49a9a7e668bf5a37c6abd8fd6
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-28 16:35:11 +0600

    port is modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require( 'express' );
 const cors = require( 'cors' );
 const ObjectId = require( 'mongodb' ).ObjectId;
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //Middleware
 app.use( cors() );
